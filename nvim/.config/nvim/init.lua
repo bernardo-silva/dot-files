524964c72f6c545f12f26af06bbcc53d6b2e1da2
Bernardo Silva <bernardo.silva_ext@mail.schwarz>
2025-01-10 16:29:35 +0000

    Update dot files

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,18 +1,2 @@
-require "user.options"
-require "user.autocommands"
-require "user.plugins"
-require "user.keymaps"
-require "user.impatient"
-require "user.cmp"
-require "user.lsp"
-require "user.treesitter"
-require "user.telescope"
-require "user.comment"
-require "user.bufferline"
-require "user.toggleterm"
-require "user.lualine"
-require "user.whichkey"
-require "user.dap"
-
-vim.g["neodark#background"] = "#121212"
-vim.cmd "colorscheme neodark"
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
---
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,11 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+return {
+  {
+    "ibhagwan/fzf-lua",
+    keys = {
+      { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (Root Dir)" },
+    },
+  },
+}
